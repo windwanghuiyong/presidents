e19bee68a2c9ff3d66615e0b11d9fdb88b256ad1
王慧勇 <weifeng19911024@163.com>
2017-01-26 13:24:32 +0800

    Initial Commit

---
+++ Presidents/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Presidents
+//
+//  Created by wanghuiyong on 26/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
